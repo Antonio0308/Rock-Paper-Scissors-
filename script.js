fbc12226d7717562f2b78bb1c569c3ddc79abb3c
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-15 15:21:05 +0300

    Implement playRound function for single round logic and score tracking
    - Add playRound function to compare player and computer choices
    - Determine round winner and increment corresponding score

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ function getHumanChoice() {
   const choiceHuman = prompt("Rock/Paper/Scissor: ").trim().toUpperCase();
   return choiceHuman;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "ROCK" && computerChoice === "PAPER") {
+    computerScore++;
+    console.log("Vous avez perdu ! Papier bat Pierre");
+  } else if (humanChoice === "ROCK" && computerChoice === "SCISSOR") {
+    humanScore++;
+    console.log("Vous avez gagné ! Pierre bat Ciseaux");
+  } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+    humanScore++;
+    console.log("Vous avez gagné ! Papier bat Pierre");
+  } else if (humanChoice === "PAPER" && computerChoice === "SCISSOR") {
+    computerScore++;
+    console.log("Vous avez perdu ! Ciseaux bat Papier");
+  } else if (humanChoice === "SCISSOR" && computerChoice === "ROCK") {
+    computerScore++;
+    console.log("Vous avez perdu ! Pierre bat Ciseaux");
+  } else if (humanChoice === "SCISSOR" && computerChoice === "PAPER") {
+    humanScore++;
+    console.log("Vous avez gagné ! Ciseaux bat Papier");
+  } else {
+    console.log("Egalité ! Même choix");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
